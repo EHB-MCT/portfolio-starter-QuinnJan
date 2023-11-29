143d408804bd3eaf227fcd00f6390c172734aa05
QuinnJan <quinn.janssens@student.ehb.be>
2023-11-29 16:45:23 +0100

    add service and router of car

--- Backend/prisma/seed.ts
+++ Backend/prisma/seed.ts
@@ -1,9 +1,18 @@
+// Add default data to database
+
 import { db } from "../src/utils/db.server";
 
 type Buyer = {
   firstName: string;
   lastName: string;
 };
+
+type Car = {
+  name: string;
+  type: string;
+  price: number;
+};
+
 async function seed() {
   getBuyers().map((buyer) => {
     return db.buyer.create({
@@ -13,6 +22,25 @@ async function seed() {
       },
     });
   });
+  const buyer = await db.buyer.findFirst({
+    where: {
+      firstName: "Tom",
+    },
+  });
+
+  if (buyer) {
+    getCars().map((car) => {
+      const { name, type, price } = car;
+      return db.car.create({
+        data: {
+          name,
+          type,
+          price,
+          buyerId: buyer.id,
+        },
+      });
+    });
+  }
 }
 function getBuyers(): Array<Buyer> {
   return [
@@ -27,6 +55,26 @@ function getBuyers(): Array<Buyer> {
     {
       firstName: "Dirk",
       lastName: "Kouters",
+    },
+  ];
+}
+
+function getCars(): Array<Car> {
+  return [
+    {
+      name: "Tesla X",
+      type: "SUV",
+      price: 120.0,
+    },
+    {
+      name: "Mercedes",
+      type: "Coupé",
+      price: 30.0,
+    },
+    {
+      name: "BMW",
+      type: "SUV",
+      price: 70.0,
     },
   ];
 }
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,19 +3,23 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 
 import { buyerRouter } from "./buyer/buyer.router";
+import { carRouter } from "./car/car.router";
 dotenv.config();
 
-if (!process.env.PORT) {
-  process.exit(1);
-}
+// if (!process.env.PORT) {
+//   process.exit(1);
+// }
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+// const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const PORT = 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/buyers", buyerRouter);
+app.use("/api/cars", carRouter);
 
 app.listen(PORT, () => {
   console.log(`Check:${PORT}`);
