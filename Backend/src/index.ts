6fb0f30fdda57c665fb443c9cc7edcc8c460e689
QuinnJan <quinn.janssens@student.ehb.be>
2024-01-03 18:26:56 +0100

    fix

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,28 +6,6 @@ import { buyerRouter } from "./buyer/buyer.router";
 import { carRouter } from "./car/car.router";
 dotenv.config();
 
-// if (!process.env.PORT) {
-//   process.exit(1);
-// }
-
-// const PORT: number = parseInt(process.env.PORT as string, 10);
-
-console.log(process.env.NODE_ENV);
-
-const config = {
-  development: {
-    databaseUrl: "",
-  },
-  production: {
-    databaseUrl: "",
-  },
-  testing: {
-    databaseUrl: "",
-  },
-};
-
-module.exports = config;
-
 const PORT = 5000;
 
 const app = express();
